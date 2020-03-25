b50930ef77ed62bd415b55bfd04304fc6693a6de
Andriamaherisoa <andriamaherisoamahandrisoa@gmail.com>
2020-03-26 00:25:07 +0100

    feat: create reducer and reducer.test files

---
+++ src/state/__test__/reducer.test.js
@@ -0,0 +1,56 @@
+import sensorReducer from '../reducer';
+import { addSensor, deleteSensor, updateSensor } from '../actions';
+
+describe('Testing main reducer', () => {
+  let initialState;
+
+  beforeEach(() => {
+    initialState = {
+      sensors: [
+        { id: 18, label: 'SENSOR_TEST_LABEL_2', value: Math.random() * 10 },
+      ],
+      selectedSensor: null,
+    };
+  });
+
+  test('should return default state', () => {
+    expect(
+      sensorReducer(initialState, () => ({
+        type: 'NOT_FOUND_ACTION_TYPE',
+        sensor: {},
+      })),
+    ).toEqual(initialState);
+  });
+
+  test('call to ADD_SENSOR action type should add new sensor', () => {
+    const newSensor = { label: 'SENSOR_TEST_LABEL', value: Math.random() * 10 };
+    expect(sensorReducer(initialState, addSensor(newSensor))).toEqual({
+      ...initialState,
+      sensors: [...initialState.sensors, newSensor],
+    });
+  });
+
+  test('call to DELETE_SENSOR action type should remove sensor', () => {
+    const firstInitialSensor = initialState.sensors[0];
+    expect(
+      sensorReducer(initialState, deleteSensor(firstInitialSensor)),
+    ).toMatchObject({
+      sensors: [],
+      selectedSensor: null,
+    });
+  });
+
+  test('call to UPDATE_SENSOR action type should update sensor', () => {
+    const newSensorValue = {
+      id: 18,
+      label: 'NEW_SENSOR_TEST_LABEL_2',
+      value: Math.random() * 10,
+    };
+    expect(
+      sensorReducer(initialState, updateSensor(newSensorValue)),
+    ).toMatchObject({
+      sensors: [newSensorValue],
+      selectedSensor: null,
+    });
+  });
+});
---
+++ src/state/reducer.js
@@ -0,0 +1,35 @@
+import { ADD_SENSOR, UPDATE_SENSOR, DELETE_SENSOR } from './types';
+
+const initialState = {
+  sensors: [],
+  selectedSensor: null,
+};
+
+/**
+ *
+ * @param {Object} state by default is our initial state
+ * @param {Object} action that contains type and sensor payload
+ */
+function sensorReducer(state = initialState, { type, sensor, selectedSensor }) {
+  switch (type) {
+    case ADD_SENSOR:
+      return { ...state, sensors: [...state.sensors, sensor] };
+
+    case UPDATE_SENSOR:
+      const sensorId = state.sensors.findIndex(s => s.id === sensor.id);
+      let newSensors = [...state.sensors];
+      newSensors[sensorId] = sensor;
+      return { ...state, sensors: newSensors };
+
+    case DELETE_SENSOR:
+      const newSensor = state.sensors.filter(s => s !== sensor);
+      return { ...state, sensors: newSensor };
+
+    case SET_SELECTED_SENSOR:
+      return { ...state, selectedSensor };
+    default:
+      return state;
+  }
+}
+
+export default sensorReducer;
