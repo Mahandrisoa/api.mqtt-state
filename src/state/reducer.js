153696db80d0e773cd3f446303ce5268aea7fe1c
Andriamaherisoa <andriamaherisoamahandrisoa@gmail.com>
2020-03-27 07:42:44 +0100

    add: add constraint to sensorReducer to handle undefined or null argument values

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -16,26 +16,26 @@ const initialState = {
  * @param {Object} action that contains type and sensor payload
  * @param {Object} selectedSensor that will hold selectedSensor
  */
-function sensorReducer(
-  state = initialState,
-  { type, sensor = {}, selectedSensor = {} },
-) {
-  switch (type) {
+function sensorReducer(state = initialState, action) {
+  if (!state || !action) {
+    return state;
+  }
+  switch (action.type) {
     case ADD_SENSOR:
-      return { ...state, sensors: [...state.sensors, sensor] };
+      return { ...state, sensors: [...state.sensors, action.sensor] };
 
     case UPDATE_SENSOR:
-      const sensorId = state.sensors.findIndex(s => s.id === sensor.id);
+      const sensorId = state.sensors.findIndex(s => s.id === action.sensor.id);
       let newSensors = [...state.sensors];
-      newSensors[sensorId] = sensor;
+      newSensors[sensorId] = action.sensor;
       return { ...state, sensors: newSensors };
 
     case DELETE_SENSOR:
-      const newSensor = state.sensors.filter(s => s !== sensor);
+      const newSensor = state.sensors.filter(s => s !== action.sensor);
       return { ...state, sensors: newSensor };
 
     case SET_SELECTED_SENSOR:
-      return { ...state, selectedSensor };
+      return { ...state, selectedSensor: action.selectedSensor };
     default:
       return state;
   }
